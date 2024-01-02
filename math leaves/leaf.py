3f3f209fee7cea1fb225f516ebeccd080ff10fbb
Emma <emmaliangren@gmail.com>
2024-01-01 20:36:20 -0500

    resources added

---
+++ math leaves/leaf.py
@@ -0,0 +1,19 @@
+import pygame
+import random 
+
+class Leaf(pygame.sprite.Sprite):
+    # a falling leaf with an arithemetic equation written on it
+    def __init__(self, x=0, y=0, num1=0, num2=0, ans=0, speed=1):
+        super().__init__()
+
+        # 3 different leaves, randomly select one to display
+        self.images = [pygame.image.load("images/leaf1.png"),pygame.image.load("images/leaf2.png"),pygame.image.load("images/leaf3.png")]
+        self.image = self.images[random.randint(0,2)]
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+        self.speed = speed
+        self.sound = pygame.mixer.Sound("audio/leaf crunch.mp3")
+        self.ans = ans
+
+    def update(self):
+        self.rect.y += self.speed 
